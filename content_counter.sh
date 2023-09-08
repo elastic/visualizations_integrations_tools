e3f73806f6552ccd477552d2b21f1fc4ec24d061
Drew Tate <drewctate@gmail.com>
2023-09-08 10:47:47 -0600

    simple content counter script

---
+++ content_counter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if a directory name was provided as a command-line argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <dashboard|lens|canvas|visualization>"
+    exit 1
+fi
+
+# Get the current directory as the root directory
+root_directory="$(pwd)/integrations"
+
+# Extract the canvas directory name from the command-line argument
+content_type="$1"
+
+# Use the find command to locate all "canvas" directories with the specified name
+find "$root_directory" -type d -name "$content_type" | while read -r content_directory; do
+    # Use dirname to get the parent directory's name
+    parent_directory=$(basename "$(dirname "$(dirname "$content_directory")")")
+    
+    # Use the find command to count the files in each "canvas" directory
+    file_count=$(find "$content_directory" -maxdepth 1 -type f | wc -l)
+    
+    # Print the package name (parent directory name) and file count
+    echo "Package: $parent_directory"
+    echo "File Count: $file_count"
+    echo ""
+done
